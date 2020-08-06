348f5ca70075799d8c97d9a72ab81e053dea7301
Mercury-cloud <drydev033@gmail.com>
2020-08-07 01:48:40 +0900

    [UI] built 3 modals for getting contact information!!!

--- pastors-line/src/App.js
+++ pastors-line/src/App.js
@@ -7,12 +7,228 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [showA, setShowA] = useState(false);
+  const [showB, setShowB] = useState(false);
+  const [showC, setShowC] = useState(false);
+  const [checked, setCheck] = useState(false);
+  const [query, setQuery] = useState('');
+  const [spinnerStyle, setSpinnerStyle] = useState({ display: 'block' });
+  const [allContacts, setAllContacts] = useState([]);
+  const [usContacts, setUSContacts] = useState([]);
+  const [detailContact, setDetailContact] = useState({});
 
+  const toggleCheckbox = () => {
+    checked ? setCheck(false) : setCheck(true);
+  }
+
+  const handleClose = () => {
+    setShowA(false);
+    setShowB(false);
+  }
+  const handleCloseC = () => {
+    setShowC(false);
+  }
+
+  const showAllContacts = () => {
+    const page = 1;
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNzEiLCJleHAiOjE2MDM3ODM0Mzd9.3ievseHtX0t3roGh7nBuNsiaQeSjfiHWyyx_5GlOLXk';
+    setSpinnerStyle({ display: 'block' });
+    setAllContacts([]);
+    axios.get(`https://api.dev.pastorsline.com/api/contacts.json`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+      params: { companyId: 171, page: page, query: query },
+
+    }).then(res => {
+      const contacts = res.data.contacts;
+      const contactArray = [];
+      for (const id in contacts) {
+        console.log(`${id}: ${contacts[id]}`);
+        contactArray.push(contacts[id]);
+      }
+      if (checked) {
+
+      }
+      console.log('all contacts--', contacts);
+      setSpinnerStyle({ display: 'none' });
+      setAllContacts(contactArray);
+    });
+  }
+  const showUSContacts = () => {
+    const page = 1;
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxNzEiLCJleHAiOjE2MDM3ODM0Mzd9.3ievseHtX0t3roGh7nBuNsiaQeSjfiHWyyx_5GlOLXk';
+    setSpinnerStyle({ display: 'block' });
+    setUSContacts([]);
+    axios.get(`https://api.dev.pastorsline.com/api/contacts.json`, {
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+      params: { companyId: 171, countryId: 226, page: page, query: query },
+    }).then(res => {
+      const contacts = res.data.contacts;
+      const contactArray = [];
+      for (const id in contacts) {
+        console.log(`${id}: ${contacts[id]}`);
+        contactArray.push(contacts[id]);
+      }
+      console.log('US contacts--', contacts);
+      setSpinnerStyle({ display: 'none' });
+      setUSContacts(contactArray);
+    });
+  }
+
+  const showDetailContact = (element) => {
+    console.log("a--->", element);
+    setDetailContact(element);
+  }
+
+  const handleShowA = () => {
+    setShowA(true);
+    showAllContacts();
+  };
+  const handleShowB = () => {
+    setShowB(true);
+    showUSContacts();
+  }
+  const handleShowC = (element) => {
+    setShowC(true);
+    showDetailContact(element);
+  }
 
 
   return (
     <div className="App">
+      <div className="main-container">
+        <Link to="/all-contacts">
+          <Button className="button-A btn" variant="primary" onClick={handleShowA}>BUTTON A</Button>
+        </Link>
 
+        <Link to="/us-contacts">
+          <Button className="button-B btn" variant="success" onClick={handleShowB}>BUTTON B</Button>
+        </Link>
+      </div>
+
+      <Modal show={showA} onHide={handleClose} dialogClassName="custom-modal" >
+        <Modal.Header closeButton>
+          <Modal.Title>All Contacts Modal A</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="search-box">
+            <p>--- Search box(name or phone number) ---</p>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="name-number">Search by</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Please write name or phone number"
+                aria-label="nameNumber"
+                aria-describedby="name-number"
+                onChange={(e) => { setQuery(e.target.value); showAllContacts() }}
+              />
+            </InputGroup>
+
+          </div>
+          <div className="contacts">
+            <Spinner animation="border" className="main-spinner" style={spinnerStyle} />
+            <ul>
+              {allContacts.map((element, index) => {
+                if (checked) {
+                  if ((index + 1) % 2 == 0)
+                    return (<li key={element.id}><a onClick={() => { handleShowC(element) }}>{`No: ${index + 1}, id: ${element.id}, name: ${element.first_name} ${element.last_name}`}</a></li>)
+                }
+                else {
+                  return (<li key={element.id}><a onClick={() => { handleShowC(element) }}>{`No: ${index + 1}, id: ${element.id}, name: ${element.first_name} ${element.last_name}`}</a></li>)
+                }
+              })}
+            </ul>
+          </div>
+          <div className="buttons">
+            <Link to="/all-contacts">
+              <Button className="button-A" onClick={() => { handleClose(); handleShowA(); }}>All Contacts</Button>
+            </Link>
+            <Link to="/us-contacts">
+              <Button className="button-B" onClick={() => { handleClose(); handleShowB(); }}>US Contacts</Button>
+            </Link>
+            <Button className="button-C" onClick={handleClose}>Close</Button>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Only even" onChange={toggleCheckbox} />
+          </Form.Group>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showB} onHide={handleClose} dialogClassName="custom-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>US Contacts Modal B</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="search-box">
+            <p>--- Search box(name or phone number) ---</p>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="name-number">Search by</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Please write name or phone number"
+                aria-label="nameNumber"
+                aria-describedby="name-number"
+                onChange={(e) => { setQuery(e.target.value); showUSContacts() }}
+              />
+            </InputGroup>
+
+          </div>
+          <div className="contacts">
+            <Spinner animation="border" className="main-spinner" style={spinnerStyle} />
+            {usContacts.map((element, index) => {
+              if (checked) {
+                if ((index + 1) % 2 == 0)
+                  return (<li key={element.id}><a onClick={() => { handleShowC(element) }}>{`No: ${index + 1}, id: ${element.id}, name: ${element.first_name} ${element.last_name}`}</a></li>)
+              }
+              else {
+                return (<li key={element.id}><a onClick={() => { handleShowC(element) }}>{`No: ${index + 1}, id: ${element.id}, name: ${element.first_name} ${element.last_name}`}</a></li>)
+              }
+            })}
+          </div>
+          <div className="buttons">
+            <Link to="/all-contacts">
+              <Button className="button-A" onClick={() => { handleClose(); handleShowA(); }}>All Contacts</Button>
+            </Link>
+            <Link to="/us-contacts">
+              <Button className="button-B" onClick={() => { handleClose(); handleShowB(); }}>US Contacts</Button>
+            </Link>
+            <Button className="button-C" onClick={handleClose}>Close</Button>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Only even" onChange={toggleCheckbox} />
+          </Form.Group>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showC} onHide={handleClose} dialogClassName="custom-modal">
+        <Modal.Header closeButton>
+          <Modal.Title>Contact Detail Modal C</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="contacts">
+            <li key={detailContact.id}>{JSON.stringify(detailContact)}</li>
+          </div>
+          <div className="buttons">
+            <Link to="/all-contacts">
+              <Button className="button-A" onClick={() => { handleClose(); handleShowA(); }}>All Contacts</Button>
+            </Link>
+            <Link to="/us-contacts">
+              <Button className="button-B" onClick={() => { handleClose(); handleShowB(); }}>US Contacts</Button>
+            </Link>
+            <Button className="button-C" onClick={handleCloseC}>Close</Button>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
