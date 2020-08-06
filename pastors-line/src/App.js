5c0a7a41e236a0eef214001ef224b03a35224e59
Mercury-cloud <drydev033@gmail.com>
2020-08-07 01:45:57 +0900

    [UI] first empty app!!!

---
+++ pastors-line/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Modal, Form, InputGroup, FormControl, Spinner } from 'react-bootstrap';
+import { Route, BrowserRouter as Router, Link } from "react-router-dom";
+import axios from 'axios';
+
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+export default App;
